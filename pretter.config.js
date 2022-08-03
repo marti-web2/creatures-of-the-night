07bfe77711852258249eb1d85409f441a3864bfd
marti-mcdee <anddagger@gmail.com>
2022-08-03 18:21:19 -0500

    improved visuals and cleaned up code

---
+++ pretter.config.js
@@ -0,0 +1,12 @@
+const options = {
+  arrowParens: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  printWidth: 120,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none",
+};
+
+module.exports = options;
--- public/input.js
+++ public/input.js
@@ -2,16 +2,20 @@
 
 export default class InputHandler {
   constructor(game) {
-    this.game = game
-    this.keys = []
-    this.listenFor = ['ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft', 'c']
-    window.addEventListener('keydown', (e) => {
+    this.game = game;
+    this.keys = [];
+    this.listenFor = ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft", "c"];
+    window.addEventListener("keydown", (e) => {
       if (this.listenFor.includes(e.key) && this.keys.indexOf(e.key) === -1) {
-        this.keys.push(e.key)
-      } else if (e.key === 'd') { this.game.debug = !this.game.debug }
-    })
-    window.addEventListener('keyup', (e) => {
-      if (this.listenFor.includes(e.key)) { this.keys.splice(this.keys.indexOf(e.key), 1) }
-    })
+        this.keys.push(e.key);
+      } else if (e.key === "d") {
+        this.game.debug = !this.game.debug;
+      }
+    });
+    window.addEventListener("keyup", (e) => {
+      if (this.listenFor.includes(e.key)) {
+        this.keys.splice(this.keys.indexOf(e.key), 1);
+      }
+    });
   }
 }
